8dd91a0204acc2778125d9b9b0243f3bdc996dc3
Victor Levesque <levesque.vic@gmail.com>
2024-01-02 21:00:51 +1100

    Added welcome Text

--- portfolio/src/components/Welcome-message.tsx
+++ portfolio/src/components/Welcome-message.tsx
@@ -6,14 +6,20 @@ const Welcome = () => {
     <div className="bg-gray-100">
       <Container>
         <div className="relative flex align-center h-screen justify-center items-center">
-          <div className="relative flex gap-x-56 justify-center items-center">
-            <div>
-              Victor Levesque
+          <div className="relative flex gap-x-48 h-[65rem] justify-center items-center">
+            <div className="relative flex flex-col max-w-xl">
+              <div className="block text-[#2d2e32] font-semibold text-6xl my-8 leading-tight">
+                Software engineer
+              </div>
+              <div className="px-4 text-lg text-[#797b82] font-medium">
+                Hi, I&apos;m Victor. Electrical Engineer by training and Software Engineer by passion. based in Melbourne 🇦🇺
+              </div>
             </div>
             <div>
               <ProfileImage/>
             </div>
           </div>
+
         </div>
       </Container>
     </div>
--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -4,11 +4,11 @@ import Container from "./ui/container";
 const Navbar = () => {
 
     return (
-      <div className="mx-auto border-b bg-white shadow-md overflow-auto">
+      <div className="fixed mx-auto border-b bg-white shadow-md w-full">
       <Container>
-          <div className="relative font-bold flex justify-between text-black text-sm lg:text-lg">
-            <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 justify-start items-center   ">
-                Test
+          <div className="relative font-bold flex justify-between h-16 text-black text-sm lg:text-lg">
+            <div className="relative px-4 text-2xl flex justify-start items-center   ">
+              &lt;VL /&gt;
             </div>
             <div className="relative font-bold px-4 sm:px-6 lg:px-8 flex h-16 justify-end items-center text-sm lg:text-lg ">
                 <Link href="/" className="ml-4 gap-x-2 ">
@@ -18,12 +18,14 @@ const Navbar = () => {
                   work
                 </Link>
                 <Link href="/" className="ml-4 gap-x-2 ">
-                  personal
+                  about
                 </Link>
                 <Link href="/" className="ml-4 gap-x-2 ">
                   blog
                 </Link>
-
+                <Link href="/" className="ml-4 gap-x-2 ">
+                  Get in touch
+                </Link>
             </div>
           </div>
       </Container>

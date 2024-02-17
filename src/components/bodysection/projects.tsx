c8b3bced85620e71c795fec64c2c9f7de6d38021
Victor Levesque <levesque.vic@gmail.com>
2024-02-17 21:28:42 +1100

    All day hikers

--- src/components/bodysection/projects.tsx
+++ src/components/bodysection/projects.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Container from "@/components/ui/container";
 import qcstore from "@/public/qcecomm-store.jpg"
-import portfolio from "@/public/portfolio.png"
+import AllDayHikers from "@/public/all-day-hikers.jpg"
 import wip from "@/public/WIP.jpeg"
 import AdminPanel from "@/public/AdminPanel.gif"
 import { IconBrandGithub,IconBrandNextjs,IconBrandPrisma,IconExternalLink,
@@ -25,101 +25,145 @@ const Projects = ({id}:ProjectsProps) => {
             </div>
             { /* -- PORTFOLIO PROJECTS --*/ }
             <div className="grid grid-cols-1 gap-20 w-full h-auto">
-              { /* -- E-COMM STORE --*/ }
-              <div className="flex flex-col p-6 mx-auto gap-8 h-[45rem] w-fit lg:flex-row lg:gap-16 lg:h-[26rem] lg:w-full overflow-hidden rounded-2xl shadow-lg bg-white">
-                <div className="w-full sm:w-[34rem] h-auto overflow-hidden rounded-2xl shadow-lg" >
-                  <a target="_blank" href="https://qcecomm-store.vercel.app/en" rel="noreferrer">
-                      <Image
-                        src={qcstore}
-                        alt="website"
-                        width={544}
-                        className="transition duration-[5000ms] hover:translate-y-[-47%]"
-                      />
-                  </a>
+            { /* -- AllDayHikers--*/ }
+            <div className="flex flex-col p-6 mx-auto gap-8 h-[45rem] w-fit lg:flex-row lg:gap-16 lg:h-[26rem] lg:w-full overflow-hidden rounded-2xl shadow-lg bg-white">
+              <div className="w-full sm:w-[34rem] h-auto overflow-hidden rounded-2xl shadow-lg" >
+                <a target="_blank" href="https://all-day-hikers.vercel.app/" rel="noreferrer">
+                    <Image
+                      src={AllDayHikers}
+                      alt="website"
+                      width={544}
+                      className="transition duration-[10000ms] hover:translate-y-[-70%]"
+                    />
+                </a>
+              </div>
+              <div className="flex flex-col h-auto items-center justify-center mx-auto w-[18rem]">
+                <div className="font-bold text-lg text-[#3a353F] mb-4">AllDayHikers (February 2024)</div>
+                <div className="text-[#797b82] font-medium text-center mb-2">A nature e-commerce front-end project. This project showcases my front-end creativity with tailCSS to make it modern and realistic.</div>
+                <div className="flex flex-row gap-4 justify-center">
+                  <div className="flex flex-row p-2 items-center">
+                    <div className="font-semibold text-[#3a353F]">Tailwind</div>
+                    <IconBrandTailwind color="#3a353F" size={24}/>
+                  </div>
+                  <div className="flex flex-row items-center">
+                    <div className="font-semibold text-[#3a353F]">React</div>
+                    <IconBrandReact color="#3a353F" size={24}/>
+                  </div>
                 </div>
-                <div className="flex flex-col h-auto items-center justify-center mx-auto w-[18rem]">
-                  <div className="font-bold text-lg text-[#3a353F] my-4">E-commerce (Decembre 2023)</div>
-                  <div className="text-[#797b82] font-medium text-center mb-2">A seamless online shopping experience for Quebecer customers in Australia offering a wide range of products. The full-stack website supports multiple languages, a shopping cart, Stripe and more.</div>
-                  <div className="flex flex-row gap-4 justify-center">
-                    <div className="flex flex-row p-2 items-center">
-                      <div className="font-semibold text-[#3a353F]">Nextjs</div>
-                      <IconBrandNextjs color="#3a353F" size={24}/>
-                    </div>
-                    <div className="flex flex-row items-center">
-                      <div className="font-semibold text-[#3a353F]">Tailwind</div>
-                      <IconBrandTailwind color="#3a353F" size={24}/>
-                    </div>
-                  </div>
-                  <div className="flex justify-between w-full mt-4 p-2 items-center">
-                    <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://github.com/VictorLev/qcecomm-store" rel="noreferrer">
-                        <div className="flex flex-row items-center rounded bg-white shadow p-1">
-                          <div className="font-semibold px-1">
-                            Code
-                          </div>
-                          <IconBrandGithub size={36}/>
+                <div className="flex justify-between w-full mt-4 p-2 items-center">
+                  <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://github.com/VictorLev/AllDayHikers" rel="noreferrer">
+                      <div className="flex flex-row items-center rounded bg-white shadow p-1">
+                        <div className="font-semibold px-1">
+                          Code
                         </div>
-                      </a>
-
-                    <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://qcecomm-store.vercel.app/en" rel="noreferrer">
-                        <div className="flex flex-row items-center rounded bg-white shadow p-1">
-                          <div className="font-semibold px-1">
-                          Live Demo
-                          </div>
-                          <IconExternalLink  size={36}/>
+                        <IconBrandGithub size={36}/>
+                      </div>
+                    </a>
+                    <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://all-day-hikers.vercel.app/" rel="noreferrer">
+                      <div className="flex flex-row items-center rounded bg-white shadow p-1">
+                        <div className="font-semibold px-1">
+                        Live Demo
                         </div>
-                      </a>
-                  </div>
+                        <IconExternalLink  size={36}/>
+                      </div>
+                    </a>
                 </div>
               </div>
+            </div>
 
-              { /* -- AdminPanel/CMS for E-COMM STORE--*/ }
-              <div className="flex flex-col p-6 mx-auto gap-8 h-[45rem] w-fit lg:flex-row-reverse lg:gap-16 lg:h-[25rem] lg:w-full overflow-hidden rounded-2xl shadow-lg bg-white">
-                <div className="relative w-full sm:w-[34rem] sm:h-full lg:h-auto overflow-hidden rounded-2xl shadow-lg" >
-                  <a target="_blank" href="https://qcecomm-admin.vercel.app" rel="noreferrer">
-                      <Image
-                        src={AdminPanel}
-                        alt="wip"
-                        width={544}
-                      />
-                  </a>
-                </div>
-                <div className="flex flex-col h-auto items-center justify-center mx-auto w-[18rem]">
-                  <div className="font-bold text-lg text-[#3a353F] mb-4 text-center">Admin Panel/CMS (Jan 2024)</div>
-                  <div className="text-[#797b82] font-medium text-center mb-2">Connected to the E-commerce Project, this back-end CMS website manages content, orders, settings, dashboards and other functionalities. This project utilized Clerk for user management, Prisma as MySQL database, and more. </div>
-
-                  <div className="flex flex-row gap-4 justify-center">
-                    <div className="flex flex-row p-2 items-center">
-                      <div className="font-semibold text-[#3a353F]">Nextjs</div>
-                      <IconBrandNextjs color="#3a353F" size={24}/>
-                    </div>
-                    <div className="flex flex-row items-center">
-                      <div className="font-semibold text-[#3a353F]">Prisma</div>
-                      <IconBrandPrisma color="#3a353F" size={24}/>
-                    </div>
-                  </div>
-                  <div className="flex justify-between w-full mt-4 p-2 items-center">
-                    <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://github.com/VictorLev/qcecomm-admin" rel="noreferrer">
-                        <div className="flex flex-row items-center rounded bg-white shadow p-1">
-                          <div className="font-semibold px-1">
-                            Code
-                          </div>
-                          <IconBrandGithub size={36}/>
-                        </div>
-                      </a>
-
-                    <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://qcecomm-admin.vercel.app" rel="noreferrer">
-                        <div className="flex flex-row items-center rounded bg-white shadow p-1">
-                          <div className="font-semibold px-1">
-                          Live Demo
-                          </div>
-                          <IconExternalLink size={36}/>
-                        </div>
-                      </a>
-                  </div>
-
-                </div>
-
+            { /* -- AdminPanel/CMS for E-COMM STORE--*/ }
+            <div className="flex flex-col p-6 mx-auto gap-8 h-[45rem] w-fit lg:flex-row-reverse lg:gap-16 lg:h-[25rem] lg:w-full overflow-hidden rounded-2xl shadow-lg bg-white">
+              <div className="relative w-full sm:w-[34rem] sm:h-full lg:h-auto overflow-hidden rounded-2xl shadow-lg" >
+                <a target="_blank" href="https://qcecomm-admin.vercel.app" rel="noreferrer">
+                    <Image
+                      src={AdminPanel}
+                      alt="wip"
+                      width={544}
+                    />
+                </a>
               </div>
+              <div className="flex flex-col h-auto items-center justify-center mx-auto w-[18rem]">
+                <div className="font-bold text-lg text-[#3a353F] mb-4 text-center">Admin Panel/CMS (Jan 2024)</div>
+                <div className="text-[#797b82] font-medium text-center mb-2">Connected to the E-commerce Project, this back-end CMS website manages content, orders, settings, dashboards and other functionalities. This project utilized Clerk for user management, Prisma as MySQL database, and more. </div>
+
+                <div className="flex flex-row gap-4 justify-center">
+                  <div className="flex flex-row p-2 items-center">
+                    <div className="font-semibold text-[#3a353F]">Nextjs</div>
+                    <IconBrandNextjs color="#3a353F" size={24}/>
+                  </div>
+                  <div className="flex flex-row items-center">
+                    <div className="font-semibold text-[#3a353F]">Prisma</div>
+                    <IconBrandPrisma color="#3a353F" size={24}/>
+                  </div>
+                </div>
+                <div className="flex justify-between w-full mt-4 p-2 items-center">
+                  <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://github.com/VictorLev/qcecomm-admin" rel="noreferrer">
+                      <div className="flex flex-row items-center rounded bg-white shadow p-1">
+                        <div className="font-semibold px-1">
+                          Code
+                        </div>
+                        <IconBrandGithub size={36}/>
+                      </div>
+                    </a>
+
+                  <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://qcecomm-admin.vercel.app" rel="noreferrer">
+                      <div className="flex flex-row items-center rounded bg-white shadow p-1">
+                        <div className="font-semibold px-1">
+                        Live Demo
+                        </div>
+                        <IconExternalLink size={36}/>
+                      </div>
+                    </a>
+                </div>
+              </div>
+            </div>
+
+            { /* -- E-COMM STORE --*/ }
+            <div className="flex flex-col p-6 mx-auto gap-8 h-[45rem] w-fit lg:flex-row lg:gap-16 lg:h-[26rem] lg:w-full overflow-hidden rounded-2xl shadow-lg bg-white">
+              <div className="w-full sm:w-[34rem] h-auto overflow-hidden rounded-2xl shadow-lg" >
+                <a target="_blank" href="https://qcecomm-store.vercel.app/en" rel="noreferrer">
+                    <Image
+                      src={qcstore}
+                      alt="website"
+                      width={544}
+                      className="transition duration-[5000ms] hover:translate-y-[-47%]"
+                    />
+                </a>
+              </div>
+              <div className="flex flex-col h-auto items-center justify-center mx-auto w-[18rem]">
+                <div className="font-bold text-lg text-[#3a353F] my-4">E-commerce (Decembre 2023)</div>
+                <div className="text-[#797b82] font-medium text-center mb-2">A seamless online shopping experience for Quebecer customers in Australia offering a wide range of products. The full-stack website supports multiple languages, a shopping cart, Stripe and more.</div>
+                <div className="flex flex-row gap-4 justify-center">
+                  <div className="flex flex-row p-2 items-center">
+                    <div className="font-semibold text-[#3a353F]">Nextjs</div>
+                    <IconBrandNextjs color="#3a353F" size={24}/>
+                  </div>
+                  <div className="flex flex-row items-center">
+                    <div className="font-semibold text-[#3a353F]">Tailwind</div>
+                    <IconBrandTailwind color="#3a353F" size={24}/>
+                  </div>
+                </div>
+                <div className="flex justify-between w-full mt-4 p-2 items-center">
+                  <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://github.com/VictorLev/qcecomm-store" rel="noreferrer">
+                      <div className="flex flex-row items-center rounded bg-white shadow p-1">
+                        <div className="font-semibold px-1">
+                          Code
+                        </div>
+                        <IconBrandGithub size={36}/>
+                      </div>
+                    </a>
+
+                  <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://qcecomm-store.vercel.app/en" rel="noreferrer">
+                      <div className="flex flex-row items-center rounded bg-white shadow p-1">
+                        <div className="font-semibold px-1">
+                        Live Demo
+                        </div>
+                        <IconExternalLink  size={36}/>
+                      </div>
+                    </a>
+                </div>
+              </div>
+            </div>
 
               { /* -- Restaurant Search Engine
               <div className="flex flex-col p-6 mx-auto gap-8 h-[45rem] w-fit lg:flex-row-reverse lg:gap-16 lg:h-[26rem] lg:w-full overflow-hidden rounded-2xl shadow-lg bg-white">
@@ -169,44 +213,6 @@ const Projects = ({id}:ProjectsProps) => {
                 </div>
 
               </div> --*/ }
-
-              { /* -- Portfolio--*/ }
-              <div className="flex flex-col p-6 mx-auto gap-8 h-[45rem] w-fit lg:flex-row lg:gap-16 lg:h-[26rem] lg:w-full overflow-hidden rounded-2xl shadow-lg bg-white">
-                <div className="w-full sm:w-[34rem] h-auto overflow-hidden rounded-2xl shadow-lg" >
-                  <a target="_blank" href="https://qcecomm-store.vercel.app/en" rel="noreferrer">
-                      <Image
-                        src={portfolio}
-                        alt="website"
-                        width={544}
-                        className="transition duration-[5000ms] hover:translate-y-[-50%]"
-                      />
-                  </a>
-                </div>
-                <div className="flex flex-col h-auto items-center justify-center mx-auto w-[18rem]">
-                  <div className="font-bold text-lg text-[#3a353F] mb-4">Portfolio (January 2024)</div>
-                  <div className="text-[#797b82] font-medium text-center mb-2">This simple yet elegant portfolio showcases my front-end capabilities with modern technologies and a focus on clarity, responsiveness and clean design.</div>
-                  <div className="flex flex-row gap-4 justify-center">
-                    <div className="flex flex-row p-2 items-center">
-                      <div className="font-semibold text-[#3a353F]">Tailwind</div>
-                      <IconBrandTailwind color="#3a353F" size={24}/>
-                    </div>
-                    <div className="flex flex-row items-center">
-                      <div className="font-semibold text-[#3a353F]">React</div>
-                      <IconBrandReact color="#3a353F" size={24}/>
-                    </div>
-                  </div>
-                  <div className="flex justify-center w-full mt-4 p-2 items-center">
-                    <a className="text-[#3a353F] hover:text-[#e89e99] transition-all" target="_blank" href="https://github.com/VictorLev/portfolio" rel="noreferrer">
-                        <div className="flex flex-row items-center rounded bg-white shadow p-1">
-                          <div className="font-semibold px-1">
-                            Code
-                          </div>
-                          <IconBrandGithub size={36}/>
-                        </div>
-                      </a>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
